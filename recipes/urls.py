b4dde319b24038f20a3ef767f833d11416fb61f9
Frank Cunha <franklinfsc41@gmail.com>
2022-04-27 16:58:26 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 # HTTP REQUEST <- HTTP RESPONSE
 # HTTP REQUEST
 
 urlpatterns = [
     path('', home),  # Home
-    path('sobre/', sobre),  # /sobre/
-    path('contato', contato),  # /contato/
 ]
